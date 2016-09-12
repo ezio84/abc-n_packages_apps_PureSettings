e56c1f2aad708ad084805e9700cc0941282c0289
ezio84 <brabus84@gmail.com>
2016-09-12 19:25:42 +0200

    Forward port 'Swap volume buttons' (3/3)

--- src/com/pure/settings/fragments/MultiMediaSettings.java
+++ src/com/pure/settings/fragments/MultiMediaSettings.java
@@ -15,9 +15,13 @@
  */
 package com.pure.settings.fragments;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceScreen;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.support.v14.preference.SwitchPreference;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
@@ -26,14 +30,39 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class MultiMediaSettings extends SettingsPreferenceFragment {
 
+    // volume rocker reorient
+    private static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+
+    private SwitchPreference mSwapVolumeButtons;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.multimedia_settings);
+
+        final ContentResolver resolver = getContentResolver();
+
+        // volume rocker reorient
+        mSwapVolumeButtons = (SwitchPreference) findPreference(SWAP_VOLUME_BUTTONS);
+        mSwapVolumeButtons.setOnPreferenceChangeListener(this);
+        int swapVolumeButtons = Settings.System.getInt(getContentResolver(),
+                SWAP_VOLUME_BUTTONS, 0);
+        mSwapVolumeButtons.setChecked(swapVolumeButtons != 0);
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.PURE_SETTINGS;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mSwapVolumeButtons) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), SWAP_VOLUME_BUTTONS,
+                    value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 }
