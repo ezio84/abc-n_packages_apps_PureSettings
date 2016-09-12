fa39c18d947e48408b0a819ddad57bd8e0bce44a
Austin T. Conn <atc3030dev@gmail.com>
2016-09-12 19:47:54 +0200

    Volume Rocker music controls [2/2]

--- src/com/pure/settings/fragments/MultiMediaSettings.java
+++ src/com/pure/settings/fragments/MultiMediaSettings.java
@@ -32,9 +32,11 @@ public class MultiMediaSettings extends SettingsPreferenceFragment {
 
     private static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
     private static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+    public static final String VOLUME_ROCKER_MUSIC_CONTROLS = "volume_rocker_music_controls";
 
     private SwitchPreference mSwapVolumeButtons;
     private SwitchPreference mVolumeRockerWake;
+    private SwitchPreference mVolumeRockerMusicControl;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,6 +58,13 @@ public class MultiMediaSettings extends SettingsPreferenceFragment {
         int volumeRockerWake = Settings.System.getInt(getContentResolver(),
                 VOLUME_ROCKER_WAKE, 0);
         mVolumeRockerWake.setChecked(volumeRockerWake != 0);
+
+        // volume rocker music control
+        mVolumeRockerMusicControl = (SwitchPreference) findPreference(VOLUME_ROCKER_MUSIC_CONTROLS);
+        mVolumeRockerMusicControl.setOnPreferenceChangeListener(this);
+        int volumeRockerMusicControl = Settings.System.getInt(getContentResolver(),
+                VOLUME_ROCKER_MUSIC_CONTROLS, 0);
+        mVolumeRockerMusicControl.setChecked(volumeRockerMusicControl != 0);
     }
 
     @Override
@@ -73,6 +82,11 @@ public class MultiMediaSettings extends SettingsPreferenceFragment {
         } else if (preference == mVolumeRockerWake) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_WAKE,
+                    value ? 1 : 0);
+            return true;
+        } else if (preference == mVolumeRockerMusicControl) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_MUSIC_CONTROLS,
                     value ? 1 : 0);
             return true;
         }
