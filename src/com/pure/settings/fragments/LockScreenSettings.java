2249e1016c9da09e2ef79db14d7a75077f9d462d
beanstown106 <nbenis106@gmail.com>
2016-10-21 10:09:34 +0200

    Fingerprint authentication vibration [2/2]

--- src/com/pure/settings/fragments/LockScreenSettings.java
+++ src/com/pure/settings/fragments/LockScreenSettings.java
@@ -15,21 +15,52 @@
  */
 package com.pure.settings.fragments;
 
+import android.content.Context;
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceScreen;
+import android.os.UserHandle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.internal.widget.LockPatternUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.pure.settings.preferences.SystemSettingSwitchPreference;
 
 public class LockScreenSettings extends SettingsPreferenceFragment {
+
+    private static final int MY_USER_ID = UserHandle.myUserId();
+
+    private static final String LS_OPTIONS_CAT = "lockscreen_options";
+    private static final String LS_SECURE_CAT = "lockscreen_secure_options";
+
+    private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
+    private FingerprintManager mFingerprintManager;
+
+    private SystemSettingSwitchPreference mFingerprintVib;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.lockscreen_settings);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        final LockPatternUtils lockPatternUtils = new LockPatternUtils(getActivity());
+
+        PreferenceCategory optionsCategory = (PreferenceCategory) findPreference(LS_OPTIONS_CAT);
+        PreferenceCategory secureCategory = (PreferenceCategory) findPreference(LS_SECURE_CAT);
+
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFingerprintVib = (SystemSettingSwitchPreference) findPreference(FINGERPRINT_VIB);
+        if (!mFingerprintManager.isHardwareDetected()){
+            secureCategory.removePreference(mFingerprintVib);
+        }
+
+        if (!lockPatternUtils.isSecure(MY_USER_ID)) {
+            prefScreen.removePreference(secureCategory);
+        }
     }
 
     @Override
