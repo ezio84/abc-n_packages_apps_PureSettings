b2f71d7525c151d22291e7bb987ec0c62ee28b2b
beanstown106 <nbenis106@gmail.com>
2016-10-25 09:49:17 +0200

    Longpress power lockscreen torch [2/2]
    DU edits :
    - Clean up a bit, remove unused imports
    - Remove switch preference if flashlight not supported

--- src/com/pure/settings/fragments/PowerSettings.java
+++ src/com/pure/settings/fragments/PowerSettings.java
@@ -21,18 +21,26 @@ import android.app.IActivityManager;
 import android.content.ContentResolver;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.internal.util.pure.DuUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class PowerSettings extends SettingsPreferenceFragment {
+public class PowerSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
 
     private static final String KEY_PROXIMITY_WAKE = "proximity_on_wake";
+    private static final String KEYGUARD_TOGGLE_TORCH = "keyguard_toggle_torch";
+
+    private SwitchPreference mKeyguardTorch;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,7 @@ public class PowerSettings extends SettingsPreferenceFragment {
         final ContentResolver resolver = activity.getContentResolver();
 
         addPreferencesFromResource(R.xml.power_settings);
+        PreferenceScreen prefSet = getPreferenceScreen();
 
         boolean proximityCheckOnWake = getResources().getBoolean(
                 com.android.internal.R.bool.config_proximityCheckOnWake);
@@ -48,6 +57,25 @@ public class PowerSettings extends SettingsPreferenceFragment {
             getPreferenceScreen().removePreference(findPreference(KEY_PROXIMITY_WAKE));
             Settings.System.putInt(resolver, Settings.System.PROXIMITY_ON_WAKE, 0);
         }
+
+        mKeyguardTorch = (SwitchPreference) findPreference(KEYGUARD_TOGGLE_TORCH);
+        mKeyguardTorch.setOnPreferenceChangeListener(this);
+        if (!DuUtils.deviceSupportsFlashLight(getActivity())) {
+            prefSet.removePreference(mKeyguardTorch);
+        } else {
+        mKeyguardTorch.setChecked((Settings.System.getInt(resolver,
+                Settings.System.KEYGUARD_TOGGLE_TORCH, 0) == 1));
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if  (preference == mKeyguardTorch) {
+            boolean checked = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.KEYGUARD_TOGGLE_TORCH, checked ? 1:0);
+            return true;
+        }
+        return false;
     }
 
     @Override
