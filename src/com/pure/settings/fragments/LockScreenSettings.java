a433296eb966d6e518c6aca89093cf88d7773358
ezio84 <brabus84@gmail.com>
2016-12-04 16:59:34 +0100

    Allow to customize max SMS per minute limit [1/2]
    Aosp has a fixed 30sms per minute value, stored in the Settings.java:
           public static final String SMS_OUTGOING_CHECK_MAX_COUNT =
                   "sms_outgoing_check_max_count";
    
    and src/java/com/android/internal/telephony/SmsUsageMonitor.java
    in the frameworks/opt/telephony:
        /** Default number of SMS sent in checking period without user permission. */
        private static final int DEFAULT_SMS_MAX_COUNT = 30;
    
    Remove the pref if the device isn't a phone.
    
    The 2/2 part is in the SmsUsageMonitor class, needed to avoid a reboot to
    update the Global Setting, and it was already made by CM guys:
    https://github.com/ezio84/abc-n_frameworks_opt_telephony/commit/dcb107746c3a5a00d64858a5f6dc46cf79a50d55

--- src/com/pure/settings/fragments/LockScreenSettings.java
+++ src/com/pure/settings/fragments/LockScreenSettings.java
@@ -16,9 +16,13 @@
 package com.pure.settings.fragments;
 
 import android.content.Context;
+import android.content.ContentResolver;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
@@ -28,19 +32,31 @@ import com.android.internal.widget.LockPatternUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settings.Utils;
 import com.pure.settings.preferences.SystemSettingSwitchPreference;
 
-public class LockScreenSettings extends SettingsPreferenceFragment {
+import java.util.List;
+import java.util.ArrayList;
+
+public class LockScreenSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener, Indexable {
 
     private static final int MY_USER_ID = UserHandle.myUserId();
 
+    private static final String APPS_SECURITY = "apps_security";
     private static final String LS_OPTIONS_CAT = "lockscreen_options";
     private static final String LS_SECURE_CAT = "lockscreen_secure_options";
 
     private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
-    private FingerprintManager mFingerprintManager;
+    private static final String SMS_OUTGOING_CHECK_MAX_COUNT = "sms_outgoing_check_max_count";
 
+    private FingerprintManager mFingerprintManager;
     private SystemSettingSwitchPreference mFingerprintVib;
+    private ListPreference mSmsCount;
+
+    private int mSmsCountValue;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,7 +64,9 @@ public class LockScreenSettings extends SettingsPreferenceFragment {
         addPreferencesFromResource(R.xml.lockscreen_settings);
         PreferenceScreen prefScreen = getPreferenceScreen();
         final LockPatternUtils lockPatternUtils = new LockPatternUtils(getActivity());
+        ContentResolver resolver = getActivity().getContentResolver();
 
+        PreferenceCategory appsSecCategory = (PreferenceCategory) findPreference(APPS_SECURITY);
         PreferenceCategory optionsCategory = (PreferenceCategory) findPreference(LS_OPTIONS_CAT);
         PreferenceCategory secureCategory = (PreferenceCategory) findPreference(LS_SECURE_CAT);
 
@@ -61,10 +79,66 @@ public class LockScreenSettings extends SettingsPreferenceFragment {
         if (!lockPatternUtils.isSecure(MY_USER_ID)) {
             prefScreen.removePreference(secureCategory);
         }
+
+        mSmsCount = (ListPreference) findPreference(SMS_OUTGOING_CHECK_MAX_COUNT);
+        mSmsCountValue = Settings.Secure.getInt(resolver,
+                Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, 30);
+        mSmsCount.setValue(Integer.toString(mSmsCountValue));
+        mSmsCount.setSummary(mSmsCount.getEntry());
+        mSmsCount.setOnPreferenceChangeListener(this);
+
+        if (!Utils.isVoiceCapable(getActivity())) {
+            appsSecCategory.removePreference(mSmsCount);
+            prefScreen.removePreference(appsSecCategory);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        // If we didn't handle it, let preferences handle it.
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        if (preference == mSmsCount) {
+            mSmsCountValue = Integer.valueOf((String) newValue);
+            int index = mSmsCount.findIndexOfValue((String) newValue);
+            mSmsCount.setSummary(
+                    mSmsCount.getEntries()[index]);
+            Settings.Global.putInt(resolver,
+                    Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, mSmsCountValue);
+            return true;
+        }
+        return false;
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.PURE_SETTINGS;
     }
-} 
+
+    public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.lockscreen_settings;
+                    result.add(sir);
+
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    ArrayList<String> result = new ArrayList<String>();
+                    return result;
+                }
+            };
+}
